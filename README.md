507079e3673177d47fc93943bf4a473403a98a39
Gabriel Habryn <gabriel.habryn@schibsted.pl>
2012-10-16 10:17:15 +0200

    readme and remove old files

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-# MustacheBundle
+# Mustache
 ## Summery
 
 Zend Framework 2 module providing integration with [mustache.php](https://github.com/bobthecow/mustache.php/wiki) rendering engine
+
+## Requirements
+
+  * Zend Framework 2 (https://github.com/zendframework/zf2). Tested on *Zend Framework 2.0.0rc6*.
+
+## Installation
+
+### Composer installation
+
+  1. `cd my/project/directory`
+  2. create a `composer.json` file with following content:
+
+     ```json
+     {
+         "require": {
+             "widmogrod/zf2-mustache-module": "dev-master"
+         }
+     }
+     ```
+  3. run `php composer.phar install`
+  4. open my/project/folder/configs/application.config.php and add 'Mustache' to your 'modules' parameter.
+
+## How to add Mustache rendering to ZF2
+
+Add to your Application module config file (module.config.php) new rendering strategy as following:
+
+```php
+<?php
+return array(
+
+    'view_manager' => array(
+        // add this
+        'strategies' => array(
+            'Mustache\View\Strategy'
+        ),
+    ),
+);
+```
--- config/module.config.php
+++ config/module.config.php
@@ -1,18 +1,12 @@
 <?php
 return array(
-    'controllers' => array(
-        'invokables' => array(
-            'Mustache\Controller\Test' => 'Mustache\Controller\TestController',
-        ),
-    ),
+
     'view_manager' => array(
-        'template_path_stack' => array(
-            'mustache' => __DIR__ . '/../view',
-        ),
         'strategies' => array(
             'Mustache\View\Strategy'
         ),
     ),
+
     'service_manager' => array(
         'factories' => array(
             'Mustache\View\Strategy' =>  'Mustache\Service\StrategyFactory',
@@ -24,23 +18,5 @@ return array(
         'suffix' => 'mustache',
         'suffixLocked' => true,
         'cache' => __DIR__ . '/../../../data/mustache'
-    ),
-
-    'router' => array(
-        'routes' => array(
-            'mustache' => array(
-                'type'    => 'segment',
-                'options' => array(
-                    'route'    => '/mustache[/:action]',
-                    'constraints' => array(
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                    ),
-                    'defaults' => array(
-                        'controller' => 'Mustache\Controller\Test',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
-        ),
     ),
 );

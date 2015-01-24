25970d09fec6937a858e6dd7c58f6d5226e3516a
Gabriel Habryn <widmogrod@gmail.com>
2015-01-24 15:30:36 +0100

    Update Module.php
    fix problem with multiple loading the Mustache module #7

--- Module.php
+++ Module.php
@@ -29,6 +29,6 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
      */
     public function getConfig()
     {
-        return include_once __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/config/module.config.php';
     }
 }
